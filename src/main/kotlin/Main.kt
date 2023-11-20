9b438039be7bb257c7c644b5ea885c60ee83f39a
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2023-11-20 14:05:10 -0300

    testando variáveis

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,17 @@
 fun main(args: Array<String>) {
+
+    val pipoca = 5;
+    val hotdog = 7;
+    var client = 10;
+
+        client += -8
+
+        println("Pipoca disponível: " + pipoca)
+        println("Hotdog disponível: "+ hotdog)
+        println("Clientes na fila: " + client)
+
+    
+
     if (args.isNotEmpty()) {
         println("Argumentos fornecidos:")
         for (arg in args) {
